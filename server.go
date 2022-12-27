a7f6798bf18cf3a40fc316cacdc2768611345953
えぼるど <ryoma@eborudoru.local>
2022-12-27 23:20:26 +0900

    エンドポイント完成

--- server.go
+++ server.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"net/http"
-	
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hello RitsLunch")
-	})
+	e.GET("/", getHello)
+
+	e.GET("/shop", getAllShops)
+
+	e.GET("/shop/:shopId", getShopDetail)
+
 	e.Logger.Fatal(e.Start(":3000"))
+}
+func getHello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello RitsLunch")
+}
+
+func getAllShops(c echo.Context) error {
+	return c.String(http.StatusOK, "店舗一覧")
+}
+
+func getShopDetail(c echo.Context) error {
+	id := c.Param("shopId")
+	return c.String(http.StatusOK, id)
 }
